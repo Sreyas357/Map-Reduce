42ffa4ccfb51d060fca24250ef4355ca69ea7805
dayaswaroopchavali <129617445+dayaswaroopchavali@users.noreply.github.com>
2024-07-14 03:50:20 +0530

    Update map_reduce.c
    minor changes done , code working fine till 1078-1156 individual letters but greater than that it throws segfault , need to care abt dat
    ->need a modularized code next to find bugs ezey

--- map_reduce.c
+++ map_reduce.c
@@ -17,7 +17,7 @@ Mapper map_f;
 Reducer reduce_f;
 
 typedef struct {
-    char key[MAX_WORD_LENGTH]; 
+    char key[MAX_WORD_LENGTH];
     char value[MAX_WORD_LENGTH];
 }pm; // structure defined for storing (key,value) pairs emitted by MR_emit
 
@@ -31,15 +31,12 @@ typedef struct{
     entry*list;
 }ht;       // structure for storing hash table
 
-//lock for workers doing map and reduce tasks
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-//lock for emiting during MR_emit
-pthread_mutex_t group_lock[MAX_GROUPS] = {PTHREAD_MUTEX_INITIALIZER};
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;   //lock for workers doing map and reduce tasks
+pthread_mutex_t group_lock[MAX_GROUPS] = {PTHREAD_MUTEX_INITIALIZER}; //lock for emiting during MR_emit
 
 pthread_cond_t master = PTHREAD_COND_INITIALIZER;
 pthread_cond_t worker = PTHREAD_COND_INITIALIZER;
-
 
 int total_mappers, number_of_mappers_done,number_of_reducers_done,total_partitions,total_keys;
 Partitioner partition_function;
@@ -47,20 +44,13 @@ Partitioner partition_function;
 
 ht *hash_table; 
 
-//memory for storing (key,value) pairs emitted by MR_emit
-pm**memory; 
+pm**memory; //memory for storing (key,value) pairs emitted by MR_emit
+int*present_index; //array which stores counter for partitions so to define memory where (key,value) pair to be stored
 
-//array which stores counter for partitions so to define memory where (key,value) pair to be stored
-int*present_index; 
+char**argv_global;  // global variable for storing argv pointer given in MR_run
 
-// global variable for storing argv pointer given in MR_run
-char**argv_global;
-
-// stores present counter used in get_next
-int*counter;  
-
-//stores all keys ,along with partion number in structure
-entry*all_keys; 
+int*counter;  // stores present counter used in get_next
+entry*all_keys; //stores all keys ,along with partion number in structure
 
 
 int compare(const void *a,const void*b){
@@ -70,7 +60,6 @@ int compare(const void *a,const void*b){
 }
 
 int*partition_on_keys(pm*array,int size){
-
     int*keys_indices = (int*)malloc(sizeof(int)*(MAX_KEYS+1));
 
     int k=1;
@@ -100,7 +89,7 @@ void*sort_pm_array(void*x){
 }
 
 
-// default partition function to group keys
+
 unsigned long default_partition(char*key,int num_partitions){
     
     unsigned long hash = 5381;
@@ -113,8 +102,6 @@ unsigned long default_partition(char*key,int num_partitions){
     }
     return hash;
 }
-
-//another hash function for partitioning
 
 unsigned int hash(const char *key) {
     unsigned long int value = 0;
@@ -143,39 +130,85 @@ int str_to_int(char*key){
  
 
 
-void* map_worker(void*x){
+// void* map_worker(void*x){
 
     
-    while(1){
+//     while(1){
         
-        pthread_mutex_lock(&lock);
+//         pthread_mutex_lock(&lock);
 
-        if( number_of_mappers_done == total_mappers){
+//         if( number_of_mappers_done == total_mappers){
+//             pthread_mutex_unlock(&lock);
+//             break;
+//         }
+        
+//         int present = number_of_mappers_done++ ;
+//         printf("Worker %d : %s\n",present+1,argv_global[present+1]);
+//         pthread_mutex_unlock(&lock);
+        
+
+//         map_f(argv_global[present+1]);
+//         printf("Worker %d : Done\n",present+1);
+
+//     }
+//     printf("Map done\n");
+//     return NULL;
+
+// }
+
+void* map_worker(void* x){
+    //pthread_mutex_t* mutex = (pthread_mutex_t*)x;
+    pthread_cond_t* cond = (pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+    if (cond == NULL) {
+        fprintf(stderr, "Failed to allocate memory for condition variable\n");
+        return NULL;
+    }
+
+    if (pthread_cond_init(cond, NULL) != 0) {
+        fprintf(stderr, "Failed to initialize condition variable\n");
+        free(cond);
+        return NULL;
+    }
+
+    while (1) {
+        pthread_mutex_lock(/*mutex*/&lock);
+
+        if (number_of_mappers_done == total_mappers) {
+            
             pthread_mutex_unlock(&lock);
-            return NULL ;
+            break;
         }
-        
-        int present = number_of_mappers_done++ ;
 
+        int present = number_of_mappers_done++;
         pthread_mutex_unlock(&lock);
-        
 
-        map_f(argv_global[present+1]);
+        if (present >= total_mappers) {
+            break; // Exit the loop if all mappers are done
+        }
+
+        map_f(argv_global[present + 1]);
+        printf("Worker %d : Done\n", present + 1);
 
     }
-    return NULL;
 
+    pthread_cond_destroy(cond);
+    free(cond);
+    return NULL;
 }
 
 
-
 void MR_emit(char*key,char*value){
-
-
+    if(key[0]=='\0' || value[0]=='\0'){
+        return;
+    }
+    printf("MR_emit : %s %s\n",key,value);
     int group_no = partition_function(key,total_partitions);
+    if(key[0]=='\0'){
+        return;
+    }
 
 
-    pm temp;
+    pm temp={0,0};
     memcpy(&temp.key,key,strlen(key)+1);
     memcpy(&temp.value,value,strlen(value)+1);
 
@@ -185,6 +218,9 @@ void MR_emit(char*key,char*value){
         present_index[group_no]++;
 
     pthread_mutex_unlock(group_lock+group_no);
+    
+    printf("MR_emit : Done %s %s\n",key,value);
+    
 
 }
 
@@ -207,35 +243,29 @@ char*get_next(char*key,int partition_number){
     return memory[partition_number][present_counter].value;
 }
 
-void* reduce_worker(void*x){
-
+void* reduce_worker(void* x) {
     pthread_mutex_lock(&lock);
 
-    while(1){
+    int present = number_of_reducers_done;
+    number_of_reducers_done += total_keys;
+    printf("Worker %d : %s\n",present+1,argv_global[present+1]);
+    pthread_mutex_unlock(&lock);
 
-        if( number_of_reducers_done == total_keys){
-            pthread_mutex_unlock(&lock);
-            return NULL ;
-        }
-        
-        int present = number_of_reducers_done++;
-
-        pthread_mutex_unlock(&lock);
-
-        reduce_f(all_keys[present].string,get_next,all_keys[present].num);
-
+    while (present < total_keys) {
+        printf("Worker %d : %s\n",present+1,all_keys[present].string);
+        reduce_f(all_keys[present].string, get_next, all_keys[present].num);
+        printf("Worker %d : %s done\n",present+1,all_keys[present].string);
+        present++;
     }
-    return NULL;
 
+    return NULL;
 }
 
 
-void MR_run(int argc , char*argv[] ,Mapper map,int num_mappers , Reducer reduce , int num_reducers,Partitioner partition,int num_partitions){
-
-    if( partition != NULL){
+void MR_run(int argc, char* argv[], Mapper map, int num_mappers, Reducer reduce, int num_reducers, Partitioner partition, int num_partitions) {
+    if (partition != NULL) {
         partition_function = partition;
-    }
-    else{
+    } else {
         partition_function = default_partition;
     }
     total_mappers = argc;
@@ -248,101 +278,86 @@ void MR_run(int argc , char*argv[] ,Mapper map,int num_mappers , Reducer reduce 
 
     total_partitions = num_partitions;
 
-    memory = ( pm**)malloc( total_partitions*sizeof(pm*));
-
-    for(int i =0 ; i< num_partitions ; i++){
-        memory[i] = ( pm*)malloc(MAX_SIZE*sizeof(pm));
+    memory = (pm**)malloc(total_partitions * sizeof(pm*));
+    for (int i = 0; i < num_partitions; i++) {
+        memory[i] = (pm*)malloc(MAX_SIZE * sizeof(pm));
     }
 
-    present_index = (int*)calloc(total_partitions,sizeof(int));
+    present_index = (int*)calloc(total_partitions, sizeof(int));
 
     pthread_t p[num_mappers];
-
-    for(int i =0 ;i < num_mappers ;i++){
-        pthread_create(&p[i],NULL,map_worker,NULL);
-    }
-    
-    for(int i =0 ;i < num_mappers ;i++){
-        pthread_join(p[i],NULL);
+    for (int i = 0; i < num_mappers; i++) {
+        pthread_create(&p[i], NULL, map_worker, NULL);
     }
 
-    //printf("mapping part done \n");
-
-    
-
-    //mapping part done ,now sorting all partition regions parralelly
+    for (int i = 0; i < num_mappers; i++) {
+        pthread_join(p[i], NULL);
+    }
 
     pthread_t w[total_partitions];
-
     int x[total_partitions];
-
-    for(int i=0;i<total_partitions;i++){x[i]=i;}
-
-    for(int i=0;i<total_partitions;i++){
-        pthread_create(&w[i],NULL,sort_pm_array,x+i);
+    for (int i = 0; i < total_partitions; i++) {
+        x[i] = i;
     }
 
-    int*partion_indices[num_partitions];
-
-    for(int i=0;i<total_partitions;i++){
-        pthread_join(w[i],(void*)(partion_indices+i));
+    for (int i = 0; i < total_partitions; i++) {
+        pthread_create(&w[i], NULL, sort_pm_array, x + i);
     }
 
-    //sorting part done
-    
+    int* partion_indices[num_partitions];
+    for (int i = 0; i < total_partitions; i++) {
+        pthread_join(w[i], (void*)(partion_indices + i));
+    }
 
-    counter = (int*)malloc(sizeof(int)*total_keys);
+    counter = (int*)malloc(sizeof(int) * total_keys);
+    hash_table = (ht*)malloc(sizeof(ht) * TABLE_SIZE);
+    all_keys = (entry*)malloc(sizeof(entry) * total_keys);
 
-    hash_table = (ht*)malloc(sizeof(ht)*TABLE_SIZE);
-    all_keys = (entry*)malloc(sizeof(entry)*total_keys);
-
-    int current=0;
-
-    for(int i = 0 ; i < num_partitions ; i++){
-
-        if(present_index[i]==0 ){continue;}
-
-        int n_k = partion_indices[i][0];
-
-
-        for(int j = 1 ; j <= n_k ; j++){
-    
-            char*key = memory[i][partion_indices[i][j]].key;
-
-
-            memcpy(all_keys[current].string,key,strlen(key)+1);
-            all_keys[current].num = i;
-
-            counter[current] = partion_indices[i][j];
-
-            int n = hash(key);
-
-            if(hash_table[n].list == NULL){
-                hash_table[n].list = (entry*)malloc(sizeof(entry)*MAX_COLLISION);
-            }
-            memcpy(hash_table[n].list[hash_table[n].p].string,key,strlen(key)+1);
-            hash_table[n].list[hash_table[n].p++].num = current++;
-
+    int current = 0;
+    for (int i = 0; i < num_partitions; i++) {
+        if (present_index[i] == 0) {
+            continue;
         }
-
+        int n_k = partion_indices[i][0];
+        for (int j = 1; j <= n_k; j++) {
+            char* key = memory[i][partion_indices[i][j]].key;
+            memcpy(all_keys[current].string, key, strlen(key) + 1);
+            all_keys[current].num = i;
+            counter[current] = partion_indices[i][j];
+            int n = hash(key);
+            if (hash_table[n].list == NULL) {
+                hash_table[n].list = (entry*)malloc(sizeof(entry) * MAX_COLLISION);
+            }
+            memcpy(hash_table[n].list[hash_table[n].p].string, key, strlen(key) + 1);
+            hash_table[n].list[hash_table[n].p++].num = current++;
+        }
     }
-
-    // for(int i=0;i<total_keys;i++){
-    //     printf("%s %d \n",all_keys[i].string,all_keys[i].num);
-    // }
-
-    //printf("reducing part started \n");
 
     number_of_reducers_done = 0;
     pthread_t r[num_reducers];
-
-    for(int i =0 ;i < num_reducers ;i++){
-        pthread_create(&r[i],NULL,reduce_worker,NULL);
+    for (int i = 0; i < num_reducers; i++) {
+        pthread_create(&r[i], NULL, reduce_worker, NULL);
     }
 
-    for(int i =0 ;i < num_reducers ;i++){
-        pthread_join(r[i],NULL);
+    for (int i = 0; i < num_reducers; i++) {
+        pthread_join(r[i], NULL);
     }
-
-
 }
+
+// void MR_run(int argc, char* argv[], Mapper map, int num_mappers, Reducer reduce, int num_reducers, Partitioner partition, int num_partitions) {
+//     if (partition != NULL) {
+//         partition_function = partition;
+//     } else {
+//         partition_function = default_partition;
+//     }
+
+//     initialize_global_variables(argc, argv, map, reduce, num_partitions);
+
+//     run_map_workers(num_mappers);
+
+//     sort_memory_arrays();
+
+//     create_hash_table();
+
+//     run_reduce_workers(num_reducers);
+// }
